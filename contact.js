f1c4b07b20a6eabe14e52bf74ca12c92df24619c
Your Name <you@example.com>
2024-06-09 23:35:57 +0200

    start the contact js

---
+++ contact.js
@@ -0,0 +1,12 @@
+const contactForm = document.querySelector("#contact-form");
+const contactSuccessAlert = document.querySelector(".contact-submit-success");
+
+const nameInput = document.querySelector("#contact-name");
+const emailInput = document.querySelector("#contact-email");
+const phoneInput = document.querySelector("#contact-phone");
+const descriptionInput = document.querySelector("#contact-description");
+
+const nameError = document.querySelector(".name-error");
+const emailError = document.querySelector(".email-error");
+const phoneError = document.querySelector(".phone-error");
+const descriptionError = document.querySelector(".description-error");
