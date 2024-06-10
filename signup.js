76824997206e1294ba1d29181215432904e6358b
eldakaliqani <eldakaliqanii@gmail.com>
2024-06-11 01:19:23 +0200

    added

---
+++ signup.js
@@ -0,0 +1,10 @@
+const signupForm = document.querySelector("#signup-form");
+const signupSuccessAlert = document.querySelector(".signup-submit-success");
+
+const usernameInput = document.querySelector("#signup-username");
+const emailInput = document.querySelector("#signup-email");
+const passwordInput = document.querySelector("#signup-password");
+
+const usernameError = document.querySelector(".username-error");
+const emailError = document.querySelector(".email-error");
+const passwordError = document.querySelector(".password-error");
