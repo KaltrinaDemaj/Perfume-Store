e241cbeb1bdddf65e09d38a801fdb52f9959f444
Your Name <you@example.com>
2024-06-10 23:16:25 +0200

    start login js

---
+++ login.js
@@ -0,0 +1,8 @@
+const loginForm = document.querySelector("#login-form");
+const loginSuccessAlert = document.querySelector(".login-submit-success");
+
+const usernameInput = document.querySelector("#login-username");
+const passwordInput = document.querySelector("#login-password");
+
+const usernameError = document.querySelector(".username-error");
+const passwordError = document.querySelector(".password-error");
